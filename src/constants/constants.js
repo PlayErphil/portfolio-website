c98deb511b2cc3b6f518b7e8fe7261a5ddbe71ac
Rene <reneleogp0305@gmail.com>
2022-02-22 00:07:43 -0500

    ML and Smart contract projects added

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,51 +1,71 @@
 export const projects = [
   {
-    title: "Python Package for crypto information",
+    title: 'Smart Contract for DeltaHacks',
     description:
-      "Built a python wrapper around LiveCoinWatch API. The wrapper is a pip python package with functions for all the endpoints.",
-    image: "/images/4.jpeg",
-    tags: ["Python", "API", "PyPi"],
-    code: "https://github.com/PlayErphil/pylivecoinwatch",
-    visit: "https://pypi.org/project/pylivecoinwatch/",
-    id: 1,
-  },
-  {
-    title: "Telegram Crypto Bot",
-    description:
-      "Built a Telegram Crypto Bot that gives you real time data about the crypto market. The bot is written in python and uses livecoinwatch.com API",
-    image: "/images/1.jpeg",
-    tags: ["Python", "Telegram API"],
-    code: "https://github.com/PlayErphil/crypto-telegram-bot",
-    visit: "https://t.me/Live_crypto_info_bot",
+      'Wrote and deployed a smart contract to an Ethereum local and test net for a hackathon project. The smart contract runs multiple elections at the same time. Also did the communication with the smart contract on the net with python.',
+    image: '/images/equalis.png',
+    tags: ['Solidity', 'Python', 'Ethereum Blockchain', 'Smart Contract'],
+    code: 'https://github.com/WSSSDC/Equalis-API',
+    visit: 'https://devpost.com/software/equalis',
     id: 2,
   },
   {
-    title: "DUC App Web API",
+    title: 'Machine Learning Project to predict the outcome of a game',
     description:
-      "Worked for DUC App as a junior developer. There I built the entire Web API Documentation page of the company. Also worked with the backend team creating the API endpoints",
-    image: "/images/2.jpeg",
-    tags: ["JavaScript", "HTML", "CSS", "Swagger", "Amazon AWS"],
-    code: "https://github.com/PlayErphil/gitlab-mirror-api",
-    visit: "https://playerphil.github.io/gitlab-mirror-api/",
+      'I implemented two machine learning algorithms to predicts the outcome of a League of Legends game.',
+    image: '/images/ml.jpeg',
+    tags: ['ML', 'Deep Neural Network', 'GBOOST', 'Python', 'Keras', 'SKlearn'],
+    code: 'https://github.com/PlayErphil/ML-Prediction-LoL',
+    visit: 'https://github.com/PlayErphil/ML-Prediction-LoL',
+    id: 1,
+  },
+  {
+    title: 'Python Package for crypto information.',
+    description:
+      'Built a python wrapper around Live Coin Watch API. The wrapper is a pip python package with functions for all the endpoints.',
+    image: '/images/4.jpeg',
+    tags: ['Python', 'PyPi'],
+    code: 'https://github.com/PlayErphil/pylivecoinwatch',
+    visit: 'https://pypi.org/project/pylivecoinwatch/',
     id: 3,
   },
   {
-    title: "Arduino to Arduino using Firebase.",
+    title: 'Telegram Crypto Bot',
     description:
-      "Internet of Things tutorial. Learn how to send sensor data from one Arduino to another using Firebase to display it onto an LCD.",
-    image: "/images/3.jpg",
-    tags: ["Arduino", "Firebase", "C++", "IOT"],
-    code: "https://github.com/PlayErphil/arduino2arduino-IOT",
-    visit:
-      "https://create.arduino.cc/projecthub/rene123456/send-sensor-data-from-one-arduino-to-another-using-firebase-c0b1b3",
+      'Built a Telegram Crypto Bot that gives you real time data about the crypto market. The bot is written in python and uses livecoinwatch.com API',
+    image: '/images/1.jpeg',
+    tags: ['Python', 'Telegram API'],
+    code: 'https://github.com/PlayErphil/crypto-telegram-bot',
+    visit: 'https://t.me/Live_crypto_info_bot',
     id: 4,
+  },
+  {
+    title: 'DUC App Web API',
+    description:
+      'Worked for DUC App as a junior developer. There I built the entire Web API Documentation page of the company. Also worked with the backend team creating the API endpoints',
+    image: '/images/2.jpeg',
+    tags: ['JavaScript', 'HTML', 'CSS', 'Swagger'],
+    code: 'https://github.com/PlayErphil/gitlab-mirror-api',
+    visit: 'https://playerphil.github.io/gitlab-mirror-api/',
+    id: 5,
+  },
+  {
+    title: 'Arduino to Arduino using Firebase.',
+    description:
+      'Internet of Things tutorial. Learn how to send sensor data from one Arduino to another using Firebase to display it onto an LCD.',
+    image: '/images/3.jpg',
+    tags: ['Arduino', 'Firebase', 'C++', 'IOT'],
+    code: 'https://github.com/PlayErphil/arduino2arduino-IOT',
+    visit:
+      'https://create.arduino.cc/projecthub/rene123456/send-sensor-data-from-one-arduino-to-another-using-firebase-c0b1b3',
+    id: 6,
   },
 ];
 
 export const TimeLineData = [
-  { year: 2017, text: "Started my journey" },
-  { year: 2018, text: "Worked as a freelance developer" },
-  { year: 2019, text: "Founded JavaScript Mastery" },
-  { year: 2020, text: "Shared my projects with the world" },
-  { year: 2021, text: "Started my own platform" },
+  { year: 2017, text: 'Started my journey' },
+  { year: 2018, text: 'Worked as a freelance developer' },
+  { year: 2019, text: 'Founded JavaScript Mastery' },
+  { year: 2020, text: 'Shared my projects with the world' },
+  { year: 2021, text: 'Started my own platform' },
 ];
